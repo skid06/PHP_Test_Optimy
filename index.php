260080a3bd88a47fb74eaf7ab40eb6a792c73b9a
Melchor Valencia <melchorvalencia@Melchors-MacBook-Pro.local>
2024-09-11 13:31:43 +0800

    Updated index file to reflect recent changes in class structure. Also add some commented out  codes to add and delete items.

--- index.php
+++ index.php
@@ -3,16 +3,32 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use Utils\NewsManager;
 use Utils\CommentManager;
+use Utils\DB;
 
-foreach (NewsManager::getInstance()->listNews() as $news) {
+$db = DB::getInstance();
+
+$commentManager = new CommentManager($db);
+$newsManager = new NewsManager($db);
+
+
+// $newsManager->addNews('Star wars V', 'Episode 5 - The Empire Strikes Back');
+// $newsManager->delete(18);
+
+foreach ($newsManager->list() as $keys => $news) {
 	echo ("############ NEWS " . $news->getTitle() . " ############\n");
 	echo ($news->getBody() . "\n");
-	foreach (CommentManager::getInstance()->listComments() as $comment) {
+
+	// if ($news->getTitle() === "Star wars V") {
+	// 	$commentManager->addCommentForNews("Strikes Back comment 1", $news->getId());
+	// 	$commentManager->addCommentForNews("Strikes Back comment 2", $news->getId());
+	// }
+
+	foreach ($commentManager->list() as $comment) {
 		if ($comment->getNewsId() == $news->getId()) {
 			echo ("Comment " . $comment->getId() . " : " . $comment->getBody() . "\n");
 		}
 	}
 }
-
-$commentManager = CommentManager::getInstance();
-$c = $commentManager->listComments();
+// $commentManager->delete(34);
+// $commentManager = CommentManager::getInstance();
+// $c = $commentManager->listComments();
